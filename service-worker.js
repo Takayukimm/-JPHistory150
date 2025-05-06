5069ad1093550ed5a8ced4e52b6b9528cd7509ab
Takayukimm <takayukimamabe@gmail.com>
2025-05-06 17:19:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
 
-const CACHE_NAME='sapix-v2';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json','./sapix_150.csv'])));});
+const CACHE_NAME='sapix-v3';
+self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json','./sapix_150.csv'])));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
